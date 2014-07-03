0c35d4a78b152e46e8b9a620c694cb6234993347
Dilyan Rusev <dilyanrusev@gmail.com>
2014-07-04 00:04:31 +0300

    add fields

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -2,14 +2,17 @@
 #include "Game.h"
 
 Game::Game()
-	: APP_TITLE(L"Falling Blocks")
-	, APP_CLASS(L"FallingBlocks")
-	, WINDOW_WIDTH(1280)
-	, WINDOW_HEIGHT(720)
-{
+		: APP_TITLE(L"Falling Blocks")
+		, APP_CLASS(L"FallingBlocks")
+		, WINDOW_WIDTH(1280)
+		, WINDOW_HEIGHT(720)
+		, BLOCKS_PER_WIDTH(10)
+		, BLOCKS_PER_HEIGHT(20) {
 }
 
+Game::~Game() {
+}
 
-Game::~Game()
-{
+void Game::Initialize() {
+
 }
--- Source/Game.h
+++ Source/Game.h
@@ -7,13 +7,30 @@ public:
 	Game();
 	~Game();
 
-private:
+	void Initialize();
+	void RunMainLoop();
+
+private:	
+	Game(const Game&);
+	Game& operator=(const Game&);
+
+	void Update(float ms);
+	void Render();
+	void CreateDeviceResources();
+	void CleanDeviceResources();
+
 	const wchar_t* APP_TITLE;
 	const wchar_t* APP_CLASS;
 	const int WINDOW_WIDTH;
 	const int WINDOW_HEIGHT;
+	const int BLOCKS_PER_WIDTH;
+	const int BLOCKS_PER_HEIGHT;
 
 	HWND m_window;
 	HINSTANCE m_instance;
+	Microsoft::WRL::ComPtr<ID2D1Factory> m_drawFactory;
+	Microsoft::WRL::ComPtr<IDWriteFactory> m_writeFactory;
+	Microsoft::WRL::ComPtr<IWICImagingFactory> m_imageFactory;
+	Microsoft::WRL::ComPtr<ID2D1HwndRenderTarget> m_renderTarget;
 };
 
