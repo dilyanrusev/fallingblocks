1f591f743cbb6a829849d3e93969a1c1105284e3
Dilyan Rusev <dilyanrusev@gmail.com>
2014-07-03 23:37:19 +0300

    add VS project & solution; add main.cpp

---
+++ Source/Main.cpp
@@ -0,0 +1,6 @@
+#include <Windows.h>
+
+int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, wchar_t* cmdLine, int cmdShow) {
+	MessageBox(nullptr, L"Compiles", L"Falling Blocks", MB_OK);
+	return 0;
+}
